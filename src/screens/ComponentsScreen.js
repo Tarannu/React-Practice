2416c22ec8c23f0bff9a047a301d1de43548365b
taranum islam <tarannum.aiub@gmail.com>
2020-12-22 00:58:36 -0500

    adding screens

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
-//Text is a primitive react element
-//view allows us to show general grouping eleent and provide styling to them
-
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the component screen</Text>;
+  const name = 'Stephen';
+
+  return (<View><Text style={styles.textStyle}>Getting started with React Native!</Text>
+    <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
+    fontSize: 45
+  },
+  subHeaderStyle: {
+    fontSize: 20
   }
 });
 
 export default ComponentsScreen;
-
-
