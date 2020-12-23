04909a69332d7132264568699f0d2ecf1c386523
taranum islam <tarannum.aiub@gmail.com>
2020-12-22 23:00:26 -0500

    new buttons and navigation and child added

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+
+const ImageDetail=()=>{
+    return (<View><Text>Image Detail</Text>
+    </View>
+    );
+
+};
+
+const styles=StyleSheet.create({
+
+});
+
+export default ImageDetail;
